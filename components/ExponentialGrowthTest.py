2ed2dd9d2181fc622d00a5c9e943b71b78586593
Jeremy Archer <open-source@fatlotus.com>
2013-05-31 12:50:09 -0500

    Remove legacy module components.

--- components/ExponentialGrowthTest.py
+++
@@ -1,8 +0,0 @@
-from components.helpers import *
-
-class ExponentialGrowthTest(Component):
-  balance  = DependentVariable("Bank account balance", requiresFirst = True)
-  interest = IndependentVariable("Bank account interest")
-  
-  def every_step(self):
-    self.balance *= (1 + self.interest * 0.01)
--- components/_template.py
+++
@@ -1,9 +0,0 @@
-from components.helpers import *
-
-class ComponentName(Component):
-  output      = DependentVariable("Output variable", requiresFirst = True)
-  external    = ExternalVariable("Variable taken from another component")
-  input       = IndependentVariable("Input parameter")
-  
-  def every_step(self):
-    self.output = self.output * self.input + self.external
